d8eb4df803d428e0a5d0e3b0c7129d30adc61c09
Lenovo SSD <Lenovo SSD@192.168.1.35>
2025-04-21 20:40:19 +0530

    comming new changes

---
+++ simpleproject/src/main/java/simpleproject/StringExample.java
@@ -0,0 +1,40 @@
+package simpleproject;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		StringBuffer buf=new StringBuffer("Hai");
+		System.out.println(buf);
+		StringBuilder sb=new StringBuilder("Welcome to kodaikanal");
+		System.out.println(sb);
+		
+		//append ->adding one string to another
+		
+		System.out.println(buf.append("   Iam Aishwarya"));
+		
+		//insert ->to insert new element based on the index
+		
+		System.out.println(buf.insert(2, "wel"));
+
+		//replace ->to replace a particular string
+		
+		buf.replace(1, 6,"elloi");
+		System.out.println(buf);
+		
+		//reverse
+		
+		/*buf.reverse();
+		System.out.println(buf);*/
+		
+		
+		//delete
+		buf.delete(0, 2);
+		System.out.println(buf);
+	}
+
+
+
+}
+
+
+
